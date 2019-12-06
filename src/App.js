436a132f7477906db3815856558ea3417ac212fa
Bcdirito <bdirito91@gmail.com>
2019-12-06 15:03:25 -0500

    implemented react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react"
+import {Switch, Route} from "react-dom"
+import Home from "./components/general/home"
 import "./App.css"
 
 const App = (props) => {
     return (
         <div className="App">
+            <Switch>
+                <Route exact path="/" render={(props) => (
+                    <Home {...props}/>
+                )}/>
+            </Switch>
             <h1>Welcome to Good Work</h1>
         </div>
     )
