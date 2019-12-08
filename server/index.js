814ecc1e33693ecd434944cfe79ac919144fb099
Bcdirito <bdirito91@gmail.com>
2019-12-08 11:07:18 -0500

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -2,16 +2,26 @@ const express = require("express")
 const path = require("path")
 const app = express()
 const cors = require("cors")
-
+const dotenv = require("dotenv")
 const port = process.env.PORT || 8000
-const MongoClient = require("mongodb").MongoClient
 
 app.use(express.static(path.join(__dirname, "../public")))
 app.use(cors())
+dotenv.config()
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+const MongoClient = require("mongodb").MongoClient
+const uri = process.env.REACT_APP_MONGO_URI
+let db;
+
+MongoClient.connect(uri, {useUnifiedTopology: true}, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db("good-work")
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`)
+    })
 })
+
+
 
 app.get("/api/test", (req, res) => {
     res.send({
