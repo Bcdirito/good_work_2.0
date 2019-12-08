c93d1fb31b447d3141160e209f84b1c5cf9ad762
Bcdirito <bdirito91@gmail.com>
2019-12-08 12:10:48 -0500

    initial home page stylings

--- src/components/general/Home.js
+++ src/components/general/Home.js
@@ -1,17 +1,11 @@
 import React from "react"
+import "../../stylesheets/home.css"
 
 const Home = (props) => {
-
-    fetch("/api/test")
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(error => console.log(error))
-
     return (
         <div className="homePage">
             <h1>Welcome to Good Work!</h1>
             <button>Log In</button>
-            <button>Sign Up</button>
             <button>Find a Doctor</button>
         </div>
     )
